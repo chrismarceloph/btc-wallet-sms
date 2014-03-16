5e7eec2adf6b525f6ef87c011abf56f298ee9b94
Kevin Chris Marcelo <chrismarceloph@gmail.com>
2014-03-16 14:43:42 +0000

    Added twilio prod settings, added ADD handler

--- wallets/tests.py
+++ wallets/tests.py
@@ -1,7 +1,9 @@
 import httpretty
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.utils import override_settings
 from .models import Account, Wallet
+from .views import ReceiveSMSView
 
 
 @httpretty.activate
@@ -31,7 +33,49 @@ class WalletModelsTest(TestCase):
         self.assertEquals(1000000, wallet_1.get_balance())
 
 
+@httpretty.activate
 class WalletViewsTest(TestCase):
+    def test_add_wallet_invalid(self):
+        guid = '59fee004-0115-4861-8e4a-7f8af3ba31fb'
+        main_password = 'password'
+        view = ReceiveSMSView()
+        view.check_wallet = lambda x, y: False
+        message = view.add_wallet('+19081000000', guid, main_password)
+        self.assertIn('Welcome', str(message))
+        self.assertIn('Sorry', str(message))
+
+    def test_add_wallet_valid(self):
+        guid = '59fee004-0115-4861-8e4a-7f8af3ba31fb'
+        main_password = 'password'
+
+        view = ReceiveSMSView()
+        view.check_wallet = lambda x, y: True
+        message = view.add_wallet('+19081000000', guid, main_password)
+        self.assertIn('Welcome', str(message))
+        self.assertIn('successfully', str(message))
+        message = view.add_wallet('+19081000000', guid, main_password)
+        self.assertNotIn('Welcome', str(message))
+        self.assertIn('already', str(message))
+
+    def test_check_wallet(self):
+        guid = '59fee004-0115-4861-8e4a-7f8af3ba31fb'
+        main_password = 'password'
+        httpretty.register_uri(
+            httpretty.GET, 'https://blockchain.info/merchant/{}/balance?password={}'.format(guid, main_password),
+            body='{"balance": 1000000}', content_type='application/json'
+        )
+
+        view = ReceiveSMSView()
+        self.assertTrue(view.check_wallet(guid, main_password))
+
+    def test_make_response(self):
+        view = ReceiveSMSView()
+        response = view.make_response('Hello!')
+        self.assertIn('Hello!', str(response))
+
+
+class WalletIntegrationTest(TestCase):
+    @override_settings(DEBUG=True)
     def test_blank_message(self):
-        response = self.client.post(reverse('receive-sms'), body='+19081000000')
-        self.assertIn(response.content, 'Sorry')
+        response = self.client.post(reverse('receive-sms'), {'From': '+19081000000', 'Body': ''})
+        self.assertIn('Sorry', response.content)
--- wallets/views.py
+++ wallets/views.py
@@ -1,6 +1,7 @@
 from twilio.twiml import Response
 from django.views.generic import View
 from django_twilio.decorators import twilio_view
+from .models import Account, Wallet
 
 
 class ReceiveSMSView(View):
@@ -16,8 +17,51 @@ class ReceiveSMSView(View):
         return self.make_response(message)
 
     def process_message(self, phone_number, body):
-        message = 'Sorry, I cannot understand your message. Please check your message and try again.'
+        parts = body.split(' ', 2)
+        if parts[0] == 'ADD':
+            if len(parts) < 3:
+                message = 'Missing parameter. Use the format ADD [wallet address] [password]'
+            else:
+                message = self.add_wallet(phone_number, parts[1], parts[2])
+        elif parts[0] == 'REM':
+            if len(parts) < 2:
+                message = 'Missing parameter. Use the format REM [wallet address]'
+            else:
+                message = self.remove_wallet(phone_number, parts[1])
+        elif parts[0] == 'BAL':
+            message = self.get_balance(phone_number)
+        else:
+            message = 'Sorry, I cannot understand your message. Please check your message and try again.'
         return message
+
+    def add_wallet(self, phone_number, guid, main_password):
+        parts = []
+        account, created = Account.objects.get_or_create(phone_number=phone_number)
+        if created:
+            parts.append('Welcome to btc-wallet-sms!')
+
+        if self.check_wallet(guid, main_password):
+            wallet, created = Wallet.objects.get_or_create(account=account, guid=guid, defaults={
+                'main_password': main_password
+            })
+            if created:
+                parts.append('You have successfully added a wallet to your account.')
+            else:
+                parts.append('You have already added this wallet to your account before.')
+        else:
+            parts.append('Sorry, your parameters did not correspond to a valid wallet.')
+        return ' '.join(parts)
+
+    def check_wallet(self, guid, main_password):
+        wallet = Wallet()
+        wallet.guid = guid
+        wallet.main_password = main_password
+        try:
+            wallet.get_balance()
+        except Exception:
+            return False
+        else:
+            return True
 
     def make_response(self, message):
         response = Response()
